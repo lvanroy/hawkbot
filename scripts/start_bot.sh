794f4ac1dcd62461d43532f7a197209e4497f521
larsvanroy <s0161083@ad.ua.ac.be>
2019-09-11 16:39:05 +0200

    fixed script

--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -2,5 +2,7 @@
 
 sudo -H pip install --upgrade pip
 sudo apt install python3-distutils
-sudo python3.6 -m pip install -r requirements.txt
+sudo apt-get install libpq-dev
+sudo apt autoremove
+sudo -H pip3.6 install -r requirements.txt
 sudo python3.6 ../Main.py
