da3582857b081d48401f3efb8260731b1ac07533
Lars Van Roy <s0161083@ad.ua.ac.be>
2021-08-08 08:32:55 +0200

    Update start_bot.sh

--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -2,5 +2,5 @@
 
 sudo -H pip3 install --upgrade pip >/dev/null
 yes | sudo apt-get autoremove >/dev/null
-sudo -H pip3 install -r /mnt/c/Users/larsv/Documents/GitHub/hawkbot/requirements.txt --upgrade >/dev/null
-sudo python3 /mnt/c/Users/larsv/Documents/GitHub/hawkbot/Main.py
+sudo -H pip3 install -r requirements.txt --upgrade >/dev/null
+sudo python3 Main.py
